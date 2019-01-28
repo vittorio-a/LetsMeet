59d1a1f8cf2a86d8138dff57d65d27ef3d4f812c
Vittorio <qdravmail@gmail.com>
2019-01-28 12:56:12 +0100

    letsmeet.sql: rimosso idComune da Posizione; aggiunto isVisible a Utente; riformattato; modifiche clausole delle chiavi esterne; SDD Dati Persistenti: effettuate le modifiche necessarie a riflettere i cambiamenti nel db (cambiato class diagram e modificate le tabelle mapping)

--- Internal work product/Database/letsmeet.sql
+++ Internal work product/Database/letsmeet.sql
@@ -1,227 +1,187 @@
-DROP database if exists letsmeet;
-create database letsmeet;
-Use letsmeet;
+DROP SCHEMA IF EXISTS letsmeet;
+CREATE SCHEMA letsmeet;
+USE letsmeet;
 
-drop table if exists Utente;
 
-create table Utente(
-	idUtente integer(11) primary key,
-    username varchar(80) unique,
-    passwordUtente binary(32),
-    email varchar(256) unique,
-	feedback float,
-    reactivationDay date
-);
-
-drop table if exists CodiceRilasciato;
-
-create table CodiceRilasciato(
-	idUtente integer(11),
-    codice smallint,
-	foreign key (idUtente) references Utente(idUtente)
-		on delete cascade on update cascade
-);
-
-drop table if exists SuperAdmin;
-
-create table SuperAdmin(
-	idSuperAdmin tinyint(8) primary key,
-	username varchar(80) unique,
-	passordAdmin binary(32)
+CREATE TABLE Utente(
+	idUtente INTEGER (11) PRIMARY KEY,
+    username VARCHAR(80) UNIQUE,
+    passwordUtente BINARY(32),
+    email VARCHAR(256) UNIQUE,
+	feedback FLOAT,
+    isVisibile BOOLEAN,
+    reactivationDay DATE
 );
 
 
-drop table if exists Tipo;
-
-create table Tipo(
-	idTipo tinyint primary key,
-    nomeTipo varchar(100),
-    descrizione varchar(100)
-);
-
-drop table if exists Posizione;
-
-create table Posizione(
-	idPosizione integer(11) primary key,
-    longitudine decimal(8,5),
-    latitudine decimal(7,5),
-    formatted_address varchar(256),
-    idComune integer(11),
-    foreign key (idComune) references Comune(idComune)
-		on delete cascade on update cascade
-);
-
-drop table if exists Evento;
-
-create table Evento(
-	idEvento integer(11) primary key,
-    nome varchar(100),
-    feedback float,
-    npartecipanti integer(11),
-    nverificati integer(11),
-    oraInizio datetime,
-    oraFine datetime,
-    idUtente integer(11),
-    idTipo tinyint ,
-    idPosizione integer(11),
-    visibile boolean,
-    foreign key (idTipo) references Tipo(idTipo)
-		on delete cascade on update cascade,
-    foreign key (idPosizione) references Posizione(idPosizione)    
-		on delete cascade on update cascade,
-    foreign key (idUtente) references Utente(idUtente)
-		on delete cascade on update cascade
+CREATE TABLE CodiceRilasciato(
+	idUtente INTEGER(11),
+    codice SMALLINT,
+    FOREIGN KEY(idUtente) REFERENCES utente(idUtente)
+		ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
-drop table if exists Rating;
-
-create table Rating(
-	idUtente integer(11),
-    idEvento integer(11),
-    voto boolean,
-    foreign key (idUtente) references Utente(idutente)
-		on delete  cascade on update cascade,
-	foreign key (idEvento) references Evento(idEvento)
-		on delete cascade on update cascade
-
-
-);
-
-drop table if exists Commento;
-
-create table Commento(
-	idCommento integer(11) primary key,
-	idMittente integer(11),
-    contenuto varchar(256),
-	idEvento integer,
-	creationTime datetime,
-    foreign key (idMittente) references Utente(idUtente)
-		on delete cascade on update cascade,
-	foreign key (idEvento) references Evento(idEvento)
-		on delete cascade on update cascade
+CREATE TABLE SuperAdmin(
+	idSuperAdmin TINYINT PRIMARY KEY,
+	username VARCHAR(80) UNIQUE,
+	passordAdmin BINARY(32)
 );
 
 
-drop table if exists Partecipazione;
-
-create table Partecipazione(
-	idUtente integer(11),
-	idEvento integer(11),
-	isVerificato boolean,
-    foreign key (idUtente) references Utente(idUtente)
-		on delete cascade on update cascade,
-    foreign key (idEvento) references Evento(idEvento)
-		on delete cascade on update cascade
+CREATE TABLE Tipo(
+	idTipo TINYINT PRIMARY KEY,
+    nomeTipo VARCHAR(100),
+    descrizione VARCHAR(100)
 );
 
-drop table if exists Segnalazione;
 
-create table Segnalazione(
-	idSegnalazione integer(11) primary key,
-	idUtente integer(11),
-    foreign key (idUtente) references Utente(idUtente)
-		on delete cascade on update cascade
+CREATE TABLE Comune(
+	idComune INTEGER(11) PRIMARY KEY,
+    nomeComune VARCHAR(256)
 );
 
-drop table if exists SegnalazioneEvento;
 
-create table SegnalazioneEvento(
-	idSegnalazione integer(11),
-    idUtente integer(11),
-	idEvento integer(11),
-    foreign key (idSegnalazione) references Segnalazione(idSegnlazione)
-		on update cascade on delete cascade,
-    foreign key (idUtente) references Utente(idUtente)
-		on update cascade on delete cascade,
-	foreign key (idEvento) references Evento(idEvento)
-		on update cascade on delete cascade
+CREATE TABLE Posizione(
+	idPosizione INTEGER(11) PRIMARY KEY,
+    longitudine DECIMAL(8,5),
+    latitudine DECIMAL(7,5),
+    formattedAddress VARCHAR(256)
 );
 
-drop table if exists SegnalazionCommento;
 
-create table SegnalazioneCommento(
-	idSegnalazione integer(11),
-    idUtente integer(11),
-	idCommento integer(11),
-    foreign key (idSegnalazione) references Segnalazione(idSegnalazione)
-		on delete cascade on update cascade,
-	foreign key (idUtente) references Utente(idUtente)
-		on delete cascade on update cascade,
-    foreign key(idCommento) references Commento(idCommento)
-		on update cascade  on delete cascade
+CREATE TABLE AppartenenzaComune(
+	idPosizione INTEGER(11),
+    idComune INTEGER(11),
+	FOREIGN KEY (idPosizione) REFERENCES Posizione(idPosizione)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idComune) REFERENCES Comune(idComune)
+		ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-drop table if exists Comune;
 
-create table Comune(
-	idComune integer(11) primary key,
-    nomeComune varchar(256)
+CREATE TABLE Provincia(
+	idProvincia INTEGER(11) PRIMARY KEY,
+    nomeProvincia VARCHAR(256),
+	sigla VARCHAR(256)
 );
 
-drop table if exists AppartenenzaComune;
 
-create table AppartenenzaComune(
-	idPosizione integer(11),
-    idComune integer(11),
-	foreign key (idPosizione) references Posizione(idPosizione)
-		on delete cascade on update cascade,
-    foreign key (idComune) references Comune(idComune)
-		on delete cascade on update cascade
+CREATE TABLE AppartenenzaProvincia(
+	idComune INTEGER(11),
+	idProvincia INTEGER(11),
+	FOREIGN KEY (idComune) REFERENCES Comune(idComune)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idProvincia) REFERENCES Provincia(idProvincia)
+		ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-drop table if exists Provincia;
 
-create table Provincia(
-	idProvincia integer(11) primary key,
-    nomeProvincia varchar(256),
-	sigla varchar(256)
+CREATE TABLE Regione(
+	idRegione INTEGER(11) PRIMARY KEY,
+	nomeRegione VARCHAR(256)
 );
 
-drop table if exists AppartenenzaProvincia;
 
-create table AppartenenzaProvincia(
-	idComune integer(11),
-	idProvincia integer(11),
-	foreign key (idComune) references Comune(idComune)
-		on delete cascade on update cascade,
-    foreign key (idProvincia) references Provincia(idProvincia)
-		on delete cascade on update cascade
+CREATE TABLE AppartenenzaRegione(
+	idRegione INTEGER(11),
+	idProvincia INTEGER(11),
+    FOREIGN KEY (idRegione) REFERENCES Regione(idRegione)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idProvincia) REFERENCES Provincia(idProvincia)
+		ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-drop table if exists Regione;
 
-create table Regione(
-	idRegione integer(11) primary key,
-	nomeRegione varchar(256)
+CREATE TABLE Nazione(
+	idNazione INTEGER(11) PRIMARY KEY,
+    nomeNazione VARCHAR(256)
 );
 
-drop table if exists AppartenenzaRegione;
 
-create table AppartenenzaRegione(
-	idRegione integer(11),
-	idProvincia integer(11),
-    foreign key (idRegione) references Regione(idRegione)
-		on update cascade on delete cascade,
-    foreign key (idProvincia) references Provincia(idProvincia)
-		on delete cascade on update cascade
-);
-
-drop table if exists Nazione;
-
-create table Nazione(
-	idNazione integer(11) primary key,
-    nomeNazione varchar(256)
-);
-
-drop table if exists  AppartenenzaNazione;
-
-create table AppartenezaNazione(
-	idRegione integer(11),
-    idNazione integer(11),
-    foreign key(idRegione) references Regione(idRegione)
-		on delete cascade on update cascade,
-    foreign key (idNazione) references Nazione(idNazione)
-		on delete cascade on update cascade
+CREATE TABLE AppartenezaNazione(
+	idRegione INTEGER(11),
+    idNazione INTEGER(11),
+    FOREIGN KEY(idRegione) REFERENCES Regione(idRegione)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idNazione) REFERENCES Nazione(idNazione)
+		ON DELETE CASCADE ON UPDATE CASCADE
 );	
 
+
+CREATE TABLE Evento(
+	idEvento INTEGER(11) PRIMARY KEY,
+    nome VARCHAR(100),
+    feedback FLOAT,
+    npartecipanti INTEGER(11),
+    nverificati INTEGER(11),
+    oraInizio DATETIME,
+    oraFine DATETIME,
+    idUtente INTEGER(11),
+    idTipo TINYINT ,
+    idPosizione INTEGER(11),
+    isVisibile BOOLEAN,
+    FOREIGN KEY(idTipo) REFERENCES Tipo(idTipo)
+		ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (idPosizione) REFERENCES Posizione(idPosizione)    
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idUtente) REFERENCES Utente(idUtente)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Rating(
+	idUtente INTEGER(11),
+    idEvento INTEGER(11),
+    voto BOOLEAN,
+    FOREIGN KEY (idUtente) REFERENCES Utente(idutente)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (idEvento) REFERENCES Evento(idEvento)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Commento(
+	idCommento INTEGER(11) PRIMARY KEY,
+	idMittente INTEGER(11),
+    contenuto VARCHAR(256),
+	idEvento INTEGER,
+	creationTime DATETIME,
+    FOREIGN KEY(idMittente) REFERENCES Utente(idUtente)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (idEvento) REFERENCES Evento(idEvento)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Partecipazione(
+	idUtente INTEGER(11),
+	idEvento INTEGER(11),
+	isVerificato BOOLEAN,
+    FOREIGN KEY (idUtente) REFERENCES Utente(idUtente)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idEvento) REFERENCES Evento(idEvento)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE SegnalazioneEvento(
+	idSegnalazione INTEGER(11),
+    idUtente INTEGER(11),
+	idEvento INTEGER(11),
+    FOREIGN KEY (idUtente) REFERENCES Utente(idUtente)
+		ON DELETE SET NULL ON UPDATE CASCADE,
+	FOREIGN KEY (idEvento) REFERENCES Evento(idEvento)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE SegnalazioneCommento(
+	idSegnalazione INTEGER(11),
+    idUtente INTEGER(11),
+	idCommento INTEGER(11),
+	FOREIGN KEY (idUtente) REFERENCES Utente(idUtente)
+		ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY(idCommento) REFERENCES Commento(idCommento)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
