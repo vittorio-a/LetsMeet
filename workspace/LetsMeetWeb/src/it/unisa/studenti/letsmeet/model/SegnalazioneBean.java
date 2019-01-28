393331b34d6af1dc94ed49d3afc9614731823cc7
Vittorio <qdravmail@gmail.com>
2019-01-28 17:44:04 +0100

    Aggiunte classi SegnalazioneBean e TipoSegnalazione

---
+++ workspace/LetsMeetWeb/src/it/unisa/studenti/letsmeet/model/SegnalazioneBean.java
@@ -0,0 +1,43 @@
+package it.unisa.studenti.letsmeet.model;
+
+/**
+ * Rappresenta una segnalazione di un item ritenuto inappropriato da un utente
+ */
+public class SegnalazioneBean {
+	private int idSegnalazione;
+	private int idItemSegnalato;
+	private TipoSegnalazione tipoSegnalazione;
+	
+/**
+ * Costruttore
+ * @param idSegnalazione l'id della segnalazione
+ * @param idItemSegnalato l'id dell'item segnalato
+ * @param tipoSegnalazione il tipo di item segnalato
+ */
+	public SegnalazioneBean(int idSegnalazione, int idItemSegnalato, TipoSegnalazione tipoSegnalazione) {
+		this.idSegnalazione = idSegnalazione;
+		this.idItemSegnalato = idItemSegnalato;
+		this.tipoSegnalazione = tipoSegnalazione;
+	}
+	
+	public TipoSegnalazione getTipoSegnalazione() {
+		return tipoSegnalazione;
+	}
+	public void setTipoSegnalazione(TipoSegnalazione tipoSegnalazione) {
+		this.tipoSegnalazione = tipoSegnalazione;
+	}
+	public int getIdItemSegnalato() {
+		return idItemSegnalato;
+	}
+	public void setIdItemSegnalato(int idItemSegnalato) {
+		this.idItemSegnalato = idItemSegnalato;
+	}
+	public int getIdSegnalazione() {
+		return idSegnalazione;
+	}
+	public void setIdSegnalazione(int idSegnalazione) {
+		this.idSegnalazione = idSegnalazione;
+	}
+	
+	
+}
---
+++ workspace/LetsMeetWeb/src/it/unisa/studenti/letsmeet/model/TipoSegnalazione.java
@@ -0,0 +1,16 @@
+package it.unisa.studenti.letsmeet.model;
+/**
+ * 
+ * Rappresenta il tipo di {@link SegnalazioneBean}
+ *
+ */
+public enum TipoSegnalazione {
+	/**
+	 * Il tipo di segnalazione per un {@link CommentoBean}
+	 */
+	COMMENTO,
+	/**
+	 * Il tipo di segnalazione per un {@link EventoBean}
+	 */
+	EVENTO
+}
