9bdc89d5022c57f5f9589a4998310d437bb852f9
Vittorio <qdravmail@gmail.com>
2019-02-28 18:30:34 +0100

    Implementato profiloControl e EventoControl risolto problema dell'internal error su searchEventControl, pulito AllFilter

--- workspace/LetsMeetWeb/src/it/unisa/studenti/letsmeet/control/gestione_account/ProfiloControl.java
+++ workspace/LetsMeetWeb/src/it/unisa/studenti/letsmeet/control/gestione_account/ProfiloControl.java
@@ -7,32 +7,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class profiloControl
- */
+
 @WebServlet("/profiloControl")
 public class ProfiloControl extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
+   
     public ProfiloControl() {
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		request.getRequestDispatcher("/profiloEsterno").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
